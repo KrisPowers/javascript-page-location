1ed189fef35c8c2c790953b9dfd28defb246f3a9
Grimm <85710701+GrimmHX@users.noreply.github.com>
2022-05-10 20:02:39 -0400

    Release [05/10/2022]

---
+++ main.js
@@ -0,0 +1,22 @@
+// Window Location Href
+document.getElementById("demo").innerHTML =
+"Page location is " + window.location.href;
+
+// Window Location Hostname
+document.getElementById("demo").innerHTML =
+"Page hostname is " + window.location.hostname;
+
+// Window Location Pathname
+document.getElementById("demo").innerHTML =
+"Page path is " + window.location.pathname;
+
+// Window Location Protocol
+document.getElementById("demo").innerHTML =
+"Page protocol is " + window.location.protocol;
+
+// Window Location Port
+document.getElementById("demo").innerHTML =
+"Port number is " + window.location.port;
+
+// Window Redirect
+window.location.replace(URL)
